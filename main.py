991c03e69fd96568a4394fd4ab98df39113d1610
guyelov <87119884+guyelov@users.noreply.github.com>
2022-06-15 13:41:23 +0300

    remove radio

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-genre = st.radio(
-     "What's your favorite movie genre",
-     ('Comedy', 'Drama', 'Documentary'))
 
-if genre == 'Comedy':
-    st.write("You select comedy.")
-else:
-     st.write("You didn't select comedy.")
 
 dataframe = pd.read_csv('player_stats_dataset.csv')
 # Set page config
