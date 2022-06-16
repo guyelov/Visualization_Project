a6e38e1ce7ea0de07987dcd469a94776e76c3d40
guyelov <87119884+guyelov@users.noreply.github.com>
2022-06-16 23:30:01 +0300

    add new data

--- main.py
+++ main.py
@@ -46,10 +46,10 @@ team_info =f' In season {selected_season}-{selected_season+1} {selected_team} pl
 try:
 
     games = int(selected_team_season['GP'])
-    str_games =  str(games) + " Matches " + " 🏟️  "
+    str_games =  str(games) + " Matches " + " 🏟️ "
     team_info+=str_games
     wins = int(selected_team_season['W'])
-    str_teams = "️Wins " + str(wins)
+    str_teams = "️ Wins " + str(wins)
     team_info+=str_teams
 
     wins = int(selected_team_season['D'])
