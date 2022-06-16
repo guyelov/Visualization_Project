1e829a71591c1496a44393e921d68677e2b2d308
guyelov <87119884+guyelov@users.noreply.github.com>
2022-06-16 23:25:03 +0300

    add new data

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ team_info =''
 try:
 
     games = int(selected_team_season['GP'])
-    str_games = "🏟️ " + str(games) + " Matches "
+    str_games = "🏟️ " + str(games) + " Matches  "
     team_info+=str_games
     wins = int(selected_team_season['W'])
     str_teams = "🏃‍♂️Wins " + str(wins)
