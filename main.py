242fce256c554a18db5f1f0e1700eed591408e9b
guyelov <87119884+guyelov@users.noreply.github.com>
2022-06-18 17:15:00 +0300

    Czechoslovakia  Dutch East Indies Netherlands Northern Ireland Serbia and Montenegro USA United Arab Emirates Wales

--- main.py
+++ main.py
@@ -161,5 +161,5 @@ else:
         height=920, margin=dict(l=0, r=0, t=0, b=0))
 
     st.plotly_chart(fig, use_container_width=False)
-img = fp.get_flag_img('Ireland')
+img = fp.get_flag_img('Serbia')
 st.image(img)
