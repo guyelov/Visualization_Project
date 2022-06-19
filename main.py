346514c4c19b926a1bcbadda7164f126e88acc09
guyelov <87119884+guyelov@users.noreply.github.com>
2022-06-19 15:30:50 +0300

    animated

--- main.py
+++ main.py
@@ -9,86 +9,86 @@ st.set_page_config(page_title='Fifa Word Cup History', page_icon=':soccer:', ini
 # matches = pd.read_csv('Data\matches.csv')
 image = Image.open('word cup wallpaper.jpg')
 
-st.image(image, caption='Sunrise by the mountains')
+st.image(image, caption='Word Cups History')
 
-leagues_stats = pd.read_csv('league_stats.csv')
-all_tables_data = pd.read_csv('all_tables.csv')
-st.markdown('# Select League ')
-selected_league = st.radio('', leagues_stats['League'], horizontal=True)
-
-row2_1, row2_spacer2, row2_2, row2_spacer3, row2_3 = st.columns((1.6, .05, 1.6, .05, 1.6))
-with row2_1:
-    unique_games_in_df = leagues_stats.loc[leagues_stats['League'] == selected_league]['GP'].iloc[0]
-    str_games = "🏟️ " + str(unique_games_in_df) + " Matches"
-    st.markdown(str_games)
-with row2_2:
-    unique_teams_in_df = leagues_stats.loc[leagues_stats['League'] == selected_league]['Num_teams'].iloc[0]
-    t = " Teams"
-    if (unique_teams_in_df == 1):
-        t = " Team"
-    str_teams = "🏃‍♂️ " + str(unique_teams_in_df) + t
-    st.markdown(str_teams)
-with row2_3:
-    total_goals_in_df = leagues_stats.loc[leagues_stats['League'] == selected_league]['GF'].iloc[0]
-    str_goals = "🥅 " + str(total_goals_in_df) + " Goals"
-    st.markdown(str_goals)
-
-teams_in_league = all_tables_data.loc[all_tables_data['League'] == selected_league]
-teams = np.unique(teams_in_league['Team'])
-seasons = np.unique(teams_in_league['Year'])
-selected_team = st.radio('Pick a Team:', teams, horizontal=True)
-team_data = teams_in_league.loc[teams_in_league['Team'] == selected_team]
-
-selected_season = st.select_slider('Choose season', seasons)
-selected_team_season = team_data.loc[team_data['Year'] == selected_season]
-
-
-def thropy(place):
-    if place == 1:
-        return ':trophy:'
-    else:
-        return ''
-
-
-team_info = f' In season {selected_season}-{selected_season + 1} {selected_team} played '
-
-try:
-
-    games = int(selected_team_season['GP'])
-    str_games = str(games) + " Matches " + " 🏟️ "
-    team_info += str_games
-    wins = int(selected_team_season['W'])
-    str_teams = " and got " + str(wins) + " Wins, "
-    team_info += str_teams
-
-    draws = int(selected_team_season['D'])
-    str_teams = str(draws) + "  Draws and  "
-    team_info += str_teams
-
-    loss = int(selected_team_season['L'])
-    str_teams = str(loss) + "  Loses."
-    team_info += str_teams
-    st.write(team_info)
-    team_info = 'They Scored '
-    goals = int(selected_team_season['GF'])
-    str_teams = str(goals) + " Goals  🥅 and conceded "
-    team_info += str_teams
-
-    conc = int(selected_team_season['GA'])
-    str_teams = str(conc) + "  Goals and finish in "
-
-    team_info += str_teams
-    place_finshed = int(selected_team_season['Place'])
-    tro = thropy(place_finshed)
-    str_goals = ' ' + str(place_finshed) + 'th' + tro + " Place."
-    team_info += str_goals
-    st.write(team_info)
-    # if place_finshed == 1:
-    #     str_goals = ":trophy:  " + str(place_finshed) + 'th' + " Place"
-    # else:
-    #     str_goals = str(place_finshed) + 'th' + " Place"
-except:
-    st.write(f'Oops.. This team was not in this league this season')
+# leagues_stats = pd.read_csv('league_stats.csv')
+# all_tables_data = pd.read_csv('all_tables.csv')
+# st.markdown('# Select League ')
+# selected_league = st.radio('', leagues_stats['League'], horizontal=True)
+#
+# row2_1, row2_spacer2, row2_2, row2_spacer3, row2_3 = st.columns((1.6, .05, 1.6, .05, 1.6))
+# with row2_1:
+#     unique_games_in_df = leagues_stats.loc[leagues_stats['League'] == selected_league]['GP'].iloc[0]
+#     str_games = "🏟️ " + str(unique_games_in_df) + " Matches"
+#     st.markdown(str_games)
+# with row2_2:
+#     unique_teams_in_df = leagues_stats.loc[leagues_stats['League'] == selected_league]['Num_teams'].iloc[0]
+#     t = " Teams"
+#     if (unique_teams_in_df == 1):
+#         t = " Team"
+#     str_teams = "🏃‍♂️ " + str(unique_teams_in_df) + t
+#     st.markdown(str_teams)
+# with row2_3:
+#     total_goals_in_df = leagues_stats.loc[leagues_stats['League'] == selected_league]['GF'].iloc[0]
+#     str_goals = "🥅 " + str(total_goals_in_df) + " Goals"
+#     st.markdown(str_goals)
+#
+# teams_in_league = all_tables_data.loc[all_tables_data['League'] == selected_league]
+# teams = np.unique(teams_in_league['Team'])
+# seasons = np.unique(teams_in_league['Year'])
+# selected_team = st.radio('Pick a Team:', teams, horizontal=True)
+# team_data = teams_in_league.loc[teams_in_league['Team'] == selected_team]
+#
+# selected_season = st.select_slider('Choose season', seasons)
+# selected_team_season = team_data.loc[team_data['Year'] == selected_season]
+#
+#
+# def thropy(place):
+#     if place == 1:
+#         return ':trophy:'
+#     else:
+#         return ''
+#
+#
+# team_info = f' In season {selected_season}-{selected_season + 1} {selected_team} played '
+#
+# try:
+#
+#     games = int(selected_team_season['GP'])
+#     str_games = str(games) + " Matches " + " 🏟️ "
+#     team_info += str_games
+#     wins = int(selected_team_season['W'])
+#     str_teams = " and got " + str(wins) + " Wins, "
+#     team_info += str_teams
+#
+#     draws = int(selected_team_season['D'])
+#     str_teams = str(draws) + "  Draws and  "
+#     team_info += str_teams
+#
+#     loss = int(selected_team_season['L'])
+#     str_teams = str(loss) + "  Loses."
+#     team_info += str_teams
+#     st.write(team_info)
+#     team_info = 'They Scored '
+#     goals = int(selected_team_season['GF'])
+#     str_teams = str(goals) + " Goals  🥅 and conceded "
+#     team_info += str_teams
+#
+#     conc = int(selected_team_season['GA'])
+#     str_teams = str(conc) + "  Goals and finish in "
+#
+#     team_info += str_teams
+#     place_finshed = int(selected_team_season['Place'])
+#     tro = thropy(place_finshed)
+#     str_goals = ' ' + str(place_finshed) + 'th' + tro + " Place."
+#     team_info += str_goals
+#     st.write(team_info)
+#     # if place_finshed == 1:
+#     #     str_goals = ":trophy:  " + str(place_finshed) + 'th' + " Place"
+#     # else:
+#     #     str_goals = str(place_finshed) + 'th' + " Place"
+# except:
+#     st.write(f'Oops.. This team was not in this league this season')
 
 # st.write(f'In season {selected_season}/{selected_season+1} ')
 
@@ -115,7 +115,7 @@ matches = pd.read_csv('WorldCupMatches.csv')
 matches.dropna(how='all', inplace=True)
 num_games = len(np.unique(matches['MatchID']))
 num_goals = df['Total_goals'].sum()
-countries =  list(np.unique(df['Team Name']))
+countries = list(np.unique(df['Team Name']))
 country_flag = {'All': 'global_flag.png', 'Germany FR': 'west germany.png', 'German DR': 'east germany.png',
                 'United Kingdom': 'uk flag.png', 'Soviet Union': 'soviet flag.png', 'Czechoslovakia': 'czech.png',
                 'Dutch East Indies': 'deind.png', 'Netherlands': 'nether.png',
@@ -136,7 +136,6 @@ with row2_1:
 with row2_2:
     list_flags = []
     if selected_country:
-
         for flag in selected_country:
             if flag in country_flag:
                 list_flags.append(country_flag[flag])
@@ -148,17 +147,7 @@ with row2_2:
                     pass
     else:
         list_flags.append('global_flag.png')
-    # try:
-    #     img = fp.get_flag_img(selected_country[0])
-    #     st.image(img)
-    # except:
-    #     try:
-    #         image = Image.open(country_flag[selected_country[0]])
-    #         st.image(image)
-    #     except:
-    #         image = Image.open(f'global_flag.png')
-    #         st.image(image)
-    st.image(list_flags,width=100)
+    st.image(list_flags, width=100)
 if not selected_country:
     data_chosen = df.loc[df['Year'] == year_chosen]
     range_color = None
