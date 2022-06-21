36ca46bc3896913a3d01aaf5f4e610bef76ac1f8
guyelov <87119884+guyelov@users.noreply.github.com>
2022-06-21 19:00:40 +0300

    animated

--- main.py
+++ main.py
@@ -192,7 +192,7 @@ df2 = worlds.loc[worlds['Year'] == year_chosen]
 # add special markers without hoverinfo
 fig.add_traces(
 go.Scatter(
-    x=np.unique(df2['Year']), y=np.unique(df2["Attendance"]), mode="markers" ,name=f'Year {year_chosen}')
+    x=np.unique(df2['Year']), y=np.unique(df2["Attendance"]), mode="markers" ,name=f'Year {int(year_chosen)}')
 )
 st.plotly_chart(fig,use_container_width=False)
 
