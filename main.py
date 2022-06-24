88561c6feb5989153ce1557795390530a077caf2
Omer Idgar <omer.idgar1@gmail.com>
2022-06-24 23:59:09 +0300

    change slider

--- main.py
+++ main.py
@@ -65,8 +65,7 @@ with row_1:
 
 _, row2_1, _, row2_2, _ = st.columns((.1, 1.6, .05, 1.6, .1))
 with row2_1:
-    st.markdown('Select some Countries')
-    selected_country = st.multiselect(None, options=countries)
+    selected_country = st.multiselect('Select some Countries', options=countries)
     s = 'You selected'
     if not selected_country:
         s += ' no specific country'
