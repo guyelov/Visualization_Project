f47c11c0108684c31bdf55980f8df8f98d5bf082
Omer Idgar <omer.idgar1@gmail.com>
2022-06-25 02:49:17 +0300

    design flags

--- main.py
+++ main.py
@@ -115,8 +115,7 @@ with row3_1:
         fig = px.choropleth(data_chosen, locations='Team Initials',
                             color="Total_goals", hover_name='Team Name',
                             color_continuous_scale='Sunsetdark',
-                            range_color=range_color,
-                            title=f'Number of goals scored by the country in {year_chosen} World Cup')
+                            range_color=range_color)
         fig.update_layout(
             margin=dict(l=0, r=0, t=0, b=0))
 
