9c5995d293ba47f76e44f967dfcd97671afde168
Omer Idgar <omer.idgar1@gmail.com>
2022-06-25 18:11:34 +0300

    design map

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ with row_1:
         cc_scale = (
                   [[0, colors[0]]]
                 + [[e, colors[(i + 1) // 2]] for i, e in enumerate(np.repeat(edges, 2))]
-                + [[1, colors[max_diffent_goals]]]
+                + [[20, colors[max_diffent_goals]]]
         )
         st.markdown(cc_scale)
         fig = px.choropleth(data_chosen, locations='Team Initials',
