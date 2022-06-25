0882eb007990fb4d9ad8d9f9157d0a4eb564fba3
Omer Idgar <omer.idgar1@gmail.com>
2022-06-25 19:10:21 +0300

    design map

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ with row_2:
     fig = px.line(worlds, x="Year", y=attribute, range_x=[1930, 2018], template="simple_white")
     fig.update_traces(textposition="bottom right")
     fig.update_layout(
-        plot_bgcolor ='rgba(255, 255, 255, 50)',
+        plot_bgcolor ='rgba(255, 255, 255, 0.1)',
         margin=dict(l=0, r=0, t=0, b=0),
         font=dict(
             family="Calibri",
