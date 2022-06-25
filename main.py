dcddca6e3437a62c837a7c4e0d3252efcc07fcc5
Omer Idgar <omer.idgar1@gmail.com>
2022-06-25 17:45:36 +0300

    design map

--- main.py
+++ main.py
@@ -138,9 +138,9 @@ with row_1:
         cc_scale = (
                   [(0, colors[0])]
                 + [(e, colors[(i + 1) // 2]) for i, e in enumerate(np.repeat(edges, 2))]
-                + [(1, colors[max_diffent_goals])]
+                + [(1, colors[max_diffent_goals-1])]
         )
-        print(cc_scale)
+
         fig = px.choropleth(data_chosen, locations='Team Initials',
                             color="Total_goals", hover_name='Team Name', hover_data=['Player Name', 'Goals Scored'],
                             color_continuous_scale=cc_scale,
